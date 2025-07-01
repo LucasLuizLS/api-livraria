ffd2f35c18e0b7235e4d3cb2d7408c019e6cda1d
LucasLuizLS <ofcl.lucasluiz@gmail.com>
2025-07-01 20:06:19 -0300

    aham

--- src/main/java/com/example/livraria/dto/LivroDTO.java
+++ src/main/java/com/example/livraria/dto/LivroDTO.java
@@ -1,4 +1,11 @@
 package com.example.livraria.dto;
 
+import lombok.Data;
+
+@Data
 public class LivroDTO {
+    private Long Id;
+    private String titulo;
+    private String autor;
+    private int ano;
 }
