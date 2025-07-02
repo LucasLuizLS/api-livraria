2d03eb22a136b8cd53c95088d21410abafe5b314
LucasLuizLS <ofcl.lucasluiz@gmail.com>
2025-07-02 00:09:02 -0300

    oba

--- src/main/java/com/example/livraria/model/Livro.java
+++ src/main/java/com/example/livraria/model/Livro.java
@@ -4,14 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+import lombok.Data; // Gera automaticamente todos os métodos "boilerplate" (Getters, Setters, etc.)
 
 @Data
-@Entity
+@Entity // Indica que essa classe é uma entidade JPA, ou seja, será mapeada para uma tabela no banco de dados
 
 public class Livro {
-    @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Id // Indica que esse campo é uma chave primaria da tabela
+    @GeneratedValue (strategy = GenerationType.IDENTITY) // Define que o valor do ID será gerado automaticamente pelo banco de dados
 
     private Long Id;
     private String titulo;
--- src/main/java/com/example/livraria/repository/LivroRepository.java
+++ src/main/java/com/example/livraria/repository/LivroRepository.java
@@ -3,5 +3,20 @@ package com.example.livraria.repository;
 import com.example.livraria.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Interface responsavel por acessar o banco de dados.
+ * Estende JpaRepository, o que fornece automaticamente
+ * métodos prontos para operações CRUD (Create, Read. Update, Delete).
+ */
+
 public interface LivroRepository extends JpaRepository<Livro, Long> {
+
+    // Nenhum metodo adicional é necessário por enquanto
+    // A JpaRepository já fornece:
+    // - findAll()
+    // - findById(Long id)
+    // - save(Livro livro)
+    // - deleteById(Long id)
+    // - existsById(Long id)
+    // - count()
 }
